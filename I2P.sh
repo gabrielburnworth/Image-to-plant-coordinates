268309fc0a3d7794d5e99e2d1b41742478e963d4
Gabriel Burnworth <gabrielburnworth@users.noreply.github.com>
2016-09-04 12:44:50 -0700

    Update I2P.sh

--- I2P.sh
+++ I2P.sh
@@ -4,18 +4,18 @@
 if [ ! $# -lt 2 ]
   then
     echo "Too many arguments supplied."
-		echo "Try enclosing the image filename in quotes."
-		exit 1
+    echo "Try enclosing the image filename in quotes."
+    exit 1
 fi
 
 # Image to process
-if [ -z "$1" ]
+if [ -z "$1" ] # no argument
   then
-    image=farmbot.png
-	else
-		image=${1// /_} # remove spaces in name
-		[ ! -f "$1" ] || [ "$1" = $image ] || mv "$1" $image  # rename without spaces
-		if [ ! "$1" = $image ]; then echo "Image renamed to $image"; fi
+    image=farmbot.png # load default image
+  else
+    image=${1// /_} # remove spaces in name
+    [ ! -f "$1" ] || [ "$1" = $image ] || mv "$1" $image  # rename without spaces if spaces exist and not done already
+    if [ ! "$1" = $image ]; then echo "Image renamed to $image"; fi # Inform that image was renamed
 fi
 
 # Input (first run only--script will use saved parameters in file if one exists)
@@ -26,10 +26,10 @@ starty=200
 n=2
 
 # Output
-name=$(echo $image | cut -d '.' -f 1)
-if [ -z "$name" ]; then exit 1; fi
+name=$(echo $image | cut -d '.' -f 1) # Name is image filename without extension(s)
+if [ -z "$name" ]; then exit 1; fi # if name is blank, exit
 dirname="$name"
-[ -d "$dirname" ] || mkdir "$dirname"
+[ -d "$dirname" ] || mkdir "$dirname" # if directory doesn't exist, create it
 trimmed=$dirname/$name'_0-trimmed.png'
 output=$dirname/$name'_1-processed.png'
 text=$dirname/$name'_2-black-pixels.txt'
@@ -37,22 +37,22 @@ coord=$dirname/$name'_3-coord.txt'
 
 # Save/load input parameters
 inputparameters=$dirname/$name'_INPUT-parameters.txt'
-if [ -f "$inputparameters" ]
-	then
-		# Load
-		size=$(sed -n '2p' < $inputparameters | cut -d "=" -f 2)
-		scale=$(sed -n '3p' < $inputparameters | cut -d "=" -f 2)
-		startx=$(sed -n '4p' < $inputparameters | cut -d "=" -f 2)
-		starty=$(sed -n '5p' < $inputparameters | cut -d "=" -f 2)
-		n=$(sed -n '6p' < $inputparameters | cut -d "=" -f 2)
-	else
-		# Save
-		echo "# input parameters for $image" >> $inputparameters
-		echo "size=$size" >> $inputparameters
-		echo "scale=$scale" >> $inputparameters
-		echo "startx=$startx" >> $inputparameters
-		echo "starty=$starty" >> $inputparameters
-		echo "n=$n" >> $inputparameters
+if [ -f "$inputparameters" ] # parameter file exists
+  then
+    # Load
+    size=$(sed -n '2p' < $inputparameters | cut -d "=" -f 2)
+    scale=$(sed -n '3p' < $inputparameters | cut -d "=" -f 2)
+    startx=$(sed -n '4p' < $inputparameters | cut -d "=" -f 2)
+    starty=$(sed -n '5p' < $inputparameters | cut -d "=" -f 2)
+    n=$(sed -n '6p' < $inputparameters | cut -d "=" -f 2)
+  else
+    # Save
+    echo "# input parameters for $image" >> $inputparameters
+    echo "size=$size" >> $inputparameters
+    echo "scale=$scale" >> $inputparameters
+    echo "startx=$startx" >> $inputparameters
+    echo "starty=$starty" >> $inputparameters
+    echo "n=$n" >> $inputparameters
 fi
 
 # Identify original image size
@@ -63,13 +63,13 @@ echo "Converting to coordinates..."
 
 # Trim image
 convert $image -background white -flatten -trim +repage \
-	-resize $originalx'x'$originaly $trimmed
+  -resize $originalx'x'$originaly $trimmed
 
 # Convert to black pixels on white background
 convert $trimmed \
-	-resize $size'x'$size \
-	-fuzz 50% -fill '#bdbdbd' -opaque black \
-	-colorspace Gray -ordered-dither o$n'x'$n $output
+  -resize $size'x'$size \
+  -fuzz 50% -fill '#bdbdbd' -opaque black \
+  -colorspace Gray -ordered-dither o$n'x'$n $output
 
 # Save pixels to text file
 convert $output $text
